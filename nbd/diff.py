aee76c838c1a24123196353249f1f461a7cf10d6
Luc Cary <luc.cary@gmail.com>
2017-09-28 16:36:36 -0400

    Handle missing RST resources, but renames are still TODO

--- nbd/diff.py
+++ nbd/diff.py
@@ -1,5 +1,8 @@
 from distutils import dir_util
 from os import path as ospath
+from subprocess import CalledProcessError
+
+import nbformat as nbf
 
 from .command import (ANSI_LIGHT_GREEN, echo)
 from .const import PKG_NAME
@@ -17,14 +20,54 @@ class DiffGenerator(object):
     self.new_commit = new_commit
     self.export_formats = export_formats
 
-  def _write_previous_version(self, filepath, output_dir, commit):
+  def _write_empty_notebook(self, output_filepath):
+    nb = nbf.v4.new_notebook()
+    with open(output_filepath, 'w') as f:
+      nbf.write(nb, f)
+    return output_filepath
+
+  class StillNoDice(Exception): pass
+
+  def _check_if_renamed(self, filepath, commit):
+    # this function should parse and run:
+    #     git log --format='%H' --name-only --follow -- demo/demo.ipynb
+    # to find renames and the commit of the rename for a given filepath.
+    pass
+
+  def _handle_git_show_128_error(self, input_filepath, filename, output_filepath, output_dir, commit):
+    # this means the file exists on disk, but not in a previous commit.
+    # In this case lets assume it's a new notebook file.
+    # It's likely a bad assumtion in some cases, but hopefully not most.
+    # TODO: handle file renames via self._check_if_renamed()
+    # try:
+    #   content = self._check_if_renamed(input_filepath, commit)
+    #   write_file(output_dir, filename, content)
+    #   return output_filepath
+    # except self.StillNoDice:
+    #   pass  # lets just write the damn empty notebook then
+    return self._write_empty_notebook(output_filepath)
+
+  def _write_previous_version(self, input_filepath, output_dir, commit):
     """
     Write a committed version of a file to a given output directory.
     """
-    content = Git.show(filepath, commit=commit)
-    filename = ospath.basename(filepath)
-    write_file(output_dir, filename, content)
-    return ospath.join(output_dir, filename)
+    filename = ospath.basename(input_filepath)
+    output_filepath = ospath.join(output_dir, filename)
+    try:
+      content = Git.show(input_filepath, commit=commit)
+    except CalledProcessError as e:
+      if e.returncode == 128:
+        return self._handle_git_show_128_error(
+          input_filepath,
+          filename,
+          output_filepath,
+          output_dir,
+          commit)
+      else:
+        raise e
+    else:
+      write_file(output_dir, filename, content)
+      return output_filepath
 
   def _export_old_and_new_notebooks(self, tempdir, old_dir, new_dir, nbformat_version):
     for filepath in self.filepaths:
