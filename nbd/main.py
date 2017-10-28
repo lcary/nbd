cd08da59e87661e8a535ff947e45bf33e495d9b6
Luc Cary <luc.cary@gmail.com>
2017-10-28 19:59:48 -0400

    modify git class usage to support unit tests

--- nbd/main.py
+++ nbd/main.py
@@ -104,7 +104,9 @@ def main():
   nb_filepaths = map(ospath.abspath, args.notebooks)
 
   # cd to the root of the git repo
-  repo_root = Git.rev_parse_show_toplevel()
+  git = Git()
+  repo_root = git.rev_parse_show_toplevel()
+
   with cd_if_necessary(repo_root):
 
     # relativize absolute filepaths to root of repo
@@ -112,6 +114,7 @@ def main():
 
     # export notebooks to various git-diff-friendly formats in tempdir
     diff_gen = DiffGenerator(
+      git,
       nb_filepaths,
       args.old_commit,
       args.new_commit,
