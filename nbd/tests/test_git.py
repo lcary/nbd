f0f8d1eb57ffc4c31bb20a18b90135f2e8adbb45
Luc Cary <luc.cary@gmail.com>
2017-10-28 21:42:37 -0400

    simplify git test cases

--- nbd/tests/test_git.py
+++ nbd/tests/test_git.py
@@ -4,45 +4,31 @@ from mock import patch
 
 def test_git_rev_parse_show_toplevel():
     git_cmd = git.Git()
-    args = [git_cmd._git, 'rev-parse', '--show-toplevel']
     with patch('nbd.git.subprocess.check_output') as mock_check_output:
         mock_check_output.return_value = "Foo\n"
         assert git_cmd.rev_parse_show_toplevel() == "Foo"
-        mock_check_output.assert_called_once_with(args)
+        mock_check_output.assert_called_once()
 
 def test_git_show():
     git_cmd = git.Git()
     filepath = 'path/to/myfile'
-    args = [git_cmd._git, 'show', '{}:{}'.format(git.HEAD, filepath)]
     with patch('nbd.git.subprocess.check_output') as mock_check_output:
         mock_check_output.return_value = "Foo\n"
         assert git_cmd.show(filepath) == "Foo\n"
-        mock_check_output.assert_called_once_with(args)
+        mock_check_output.assert_called_once()
 
 def test_git_diff_no_index():
     git_cmd = git.Git()
     filepath1 = 'path/to/myfile_1'
     filepath2 = 'path/to/myfile_2'
-
-    args = [
-        git_cmd._git,
-        '--no-pager',
-        'diff',
-        '--exit-code',
-        '--no-index',
-        '--color=always',
-        filepath1,
-        filepath2]
-
     with patch('nbd.git.subprocess.call') as mock_call:
         git_cmd.diff_no_index(filepath1, filepath2)
-        mock_call.assert_called_once_with(args)
+        mock_call.assert_called_once()
 
 def test_git_diff_name_status():
     git_cmd = git.Git()
     filepath = 'path/to/myfile'
-    args = [git_cmd._git, 'diff', filepath, '--name-status']
     with patch('nbd.git.subprocess.check_output') as mock_check_output:
         mock_check_output.return_value = "Foo\n"
         assert git_cmd.diff_name_status(filepath) == "Foo\n"
-        mock_check_output.assert_called_once_with(args)
+        mock_check_output.assert_called_once()
