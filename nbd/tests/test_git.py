caa00d9bd93e336b47b675579607d1df2b4b5e5c
Luc Cary <luc.cary@gmail.com>
2017-10-28 21:38:17 -0400

    update git tests, replacing stubs with mocks

--- nbd/tests/test_git.py
+++ nbd/tests/test_git.py
@@ -1,47 +1,48 @@
 from .context import git
 
-import pytest
+from mock import patch
 
-@pytest.fixture
-def command():
-    class Command(object):
-        def __init__(self):
-            self.last_call = None
-        def join(self, args):
-            return ' '.join(args)
-        def call(self, args):
-            result = self.join(args)
-            self.last_call = result
-        def check_output(self, args):
-            result = self.join(args)
-            self.last_call = result
-            return result
-    return Command()
+def test_git_rev_parse_show_toplevel():
+    git_cmd = git.Git()
+    args = [git_cmd._git, 'rev-parse', '--show-toplevel']
+    with patch('nbd.git.subprocess.check_output') as mock_check_output:
+        mock_check_output.return_value = "Foo\n"
+        assert git_cmd.rev_parse_show_toplevel() == "Foo"
+        mock_check_output.assert_called_once_with(args)
 
-def test_git_rev_parse_show_toplevel(command):
-    git_cmd = git.Git(command=command)
-    expect = '{} rev-parse --show-toplevel'.format(git_cmd._git)
-    assert git_cmd.rev_parse_show_toplevel() == expect
-
-def test_git_show(command):
-    git_cmd = git.Git(command=command)
+def test_git_show():
+    git_cmd = git.Git()
     filepath = 'path/to/myfile'
-    expect = '{} show {}:{}'.format(git_cmd._git, git.HEAD, filepath)
-    assert git_cmd.show(filepath) == expect
+    args = [git_cmd._git, 'show', '{}:{}'.format(git.HEAD, filepath)]
+    with patch('nbd.git.subprocess.check_output') as mock_check_output:
+        mock_check_output.return_value = "Foo\n"
+        assert git_cmd.show(filepath) == "Foo\n"
+        mock_check_output.assert_called_once_with(args)
 
-def test_git_diff_no_index(command):
-    git_cmd = git.Git(command=command)
-    raw_str = '{} --no-pager diff --exit-code --no-index --color=always {} {}'
+def test_git_diff_no_index():
+    git_cmd = git.Git()
     filepath1 = 'path/to/myfile_1'
     filepath2 = 'path/to/myfile_2'
-    expect = raw_str.format(git_cmd._git, filepath1, filepath2)
-    git_cmd.diff_no_index(filepath1, filepath2)
-    assert command.last_call == expect
-    git_cmd.diff_no_index(filepath2, filepath1)
-    assert command.last_call != expect
 
-def test_git_diff_name_status(command):
-    git_cmd = git.Git(command=command)
+    args = [
+        git_cmd._git,
+        '--no-pager',
+        'diff',
+        '--exit-code',
+        '--no-index',
+        '--color=always',
+        filepath1,
+        filepath2]
+
+    with patch('nbd.git.subprocess.call') as mock_call:
+        git_cmd.diff_no_index(filepath1, filepath2)
+        mock_call.assert_called_once_with(args)
+
+def test_git_diff_name_status():
+    git_cmd = git.Git()
     filepath = 'path/to/myfile'
-    expect = '{} diff {} --name-status'.format(git_cmd._git, filepath)
-    assert git_cmd.diff_name_status(filepath) == expect
+    args = [git_cmd._git, 'diff', filepath, '--name-status']
+    with patch('nbd.git.subprocess.check_output') as mock_check_output:
+        mock_check_output.return_value = "Foo\n"
+        assert git_cmd.diff_name_status(filepath) == "Foo\n"
+        mock_check_output.assert_called_once_with(args)
