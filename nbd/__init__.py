0ac5763e4f1d74243ce8ff0f563a90bf25bc1233
Luc Cary <luc.cary@gmail.com>
2017-10-29 13:45:31 -0400

    bump version again

--- nbd/__init__.py
+++ nbd/__init__.py
@@ -1,3 +1,3 @@
 # Why this file? https://packaging.python.org/guides/single-sourcing-package-version/
 # How is version set? https://www.python.org/dev/peps/pep-0440/
-__version__ = '0.1.dev6'
+__version__ = '0.1.dev7'
